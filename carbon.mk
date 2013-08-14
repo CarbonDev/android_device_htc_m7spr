16d8087e8e58d78bdb45b82aa95166d2af5d136a
Daniel MM <daxx4182@gmail.com>
2013-08-14 12:51:55 +0800

    Carbonize

--- carbon.mk
+++ carbon.mk
@@ -1,12 +1,11 @@
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7wls/m7wls:4.1.2/JZO54K/166937.7:user/release-keys" PRIVATE_BUILD_DESC="1.29.651.7 CL166937 release-keys"
 
-PRODUCT_NAME := cm_m7spr
+PRODUCT_NAME := carbon_m7spr
 PRODUCT_DEVICE := m7spr
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.m7spr.$(shell date +%m%d%y).$(shell date +%H%M%S)
